80a890e9784304321b0ae88fc0c2a67e2410b2e4
KuldeepJha5176 <ranacjha@gmail.com>
2025-07-28 01:23:18 +0530

    completed the timeline

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -3,14 +3,14 @@
 import { useRef } from "react";
 import React from "react";
 import { useInView, motion } from "motion/react";
-import { i } from "framer-motion/client";
+import { IconCircleCheckFilled } from "@tabler/icons-react";
+import { cn } from "@/lib/utils";
 
 type Data = {
   title: string;
   content: Array<{
     title: string;
     description?: string;
-    icon?: string;
   }>;
 };
 export const Timeline = () => {
@@ -22,10 +22,9 @@ export const Timeline = () => {
       title: "2022",
       content: [
         {
-          title: "Graduated from University of Delhi",
+          title: "Graduated from NIET",
           description:
-            "I graduated from University of Delhi with a Bachelor's degree in Computer Science and Engineering.",
-          icon: "🎓",
+            "I graduated from NIET with a Bachelor's degree in Computer Science and Engineering.",
         },
       ],
     },
@@ -36,7 +35,6 @@ export const Timeline = () => {
           title: "Completed my internship at Figma",
           description:
             "I interned at Figma as a Software Engineer, where I worked on various projects related to design systems and user experience.",
-          icon: "💻",
         },
       ],
     },
@@ -47,7 +45,6 @@ export const Timeline = () => {
           title: "Completed my internship at Google",
           description:
             "I interned at Google as a Software Engineer, where I worked on various projects related to web development and data analysis.",
-          icon: "💻",
         },
       ],
     },
@@ -58,7 +55,6 @@ export const Timeline = () => {
           title: "Completed my internship at Adobe",
           description:
             "I interned at Adobe as a Software Engineer, where I worked on various projects related to web development and user experience.",
-          icon: "💻",
         },
       ],
     },
@@ -69,16 +65,15 @@ export const Timeline = () => {
           title: "Completed my internship at Microsoft",
           description:
             "I interned at Microsoft as a Software Engineer, where I worked on various projects related to web development and user experience.",
-          icon: "💻",
         },
       ],
     },
   ];
 
   return (
-    <div ref={ref} className="flex flex-col items-center justify-center">
+    <div ref={ref} className="flex flex-col items-start justify-center">
       {timeline.map((yearofStudy, idx) => (
-        <div key={idx}>
+        <div key={idx} className="mb-4">
           <motion.h2
             animate={{
               filter: isInView ? "blur(0px)" : "blur(10px)",
@@ -89,44 +84,82 @@ export const Timeline = () => {
               ease: "easeInOut",
               delay: 0.1 * idx,
             }}
-            className="font-bold text-black"
+            className="shadow-custom w-fit rounded-md px-2 py-0.5 mb-2 font-bold text-black"
           >
             {yearofStudy.title}
           </motion.h2>
-          <ul>
-            {yearofStudy.content.map((item, idx) => (
-              <li key={idx}>
-                <div className="pl-4">
-                  <motion.span
-                  animate={{
-                    filter: isInView ? "blur(0px)" : "blur(10px)",
-                    opacity: isInView ? 1 : 0,
-                    y: isInView ? 0 : 10,
-                    transition: {
-                      duration: 0.3,
-                      ease: "easeInOut",
-                      delay: 0.1 * idx,
-                    },
-                  }} 
-                  >{item.icon}</motion.span>
-                  <motion.h3 
-                  animate={{
-                    opacity: isInView ? 1 : 0,
-                    y: isInView ? 0 : 10,
-                    transition: {
-                      duration: 0.3,
-                      ease: "easeInOut",
-                      delay: 0.2 * idx,
-                    },
-                  }}className="text-neutral-800 dark:text-neutral-200"
-                  >{item.title}</motion.h3>
-                  {item.description && <p className="pt-2 text-neutral-600 dark:text-neutral-400">{item.description}</p>}
-                </div>
-              </li>
-            ))}
-          </ul>
+          <div className="flex flex-col gap-4">
+            <ul className="list-none pl-4">
+              {yearofStudy.content.map((item, idx) => (
+                <li key={idx}>
+                  <Step className="flex gap-2" idx={idx} isInView={isInView}>
+                    <motion.h3
+                      animate={{
+                        opacity: isInView ? 1 : 0,
+                        y: isInView ? 0 : 10,
+                        transition: {
+                          duration: 0.3,
+                          ease: "easeInOut",
+                          delay: 0.2 * idx,
+                        },
+                      }}
+                      className=" text-neutral-800 dark:text-neutral-200"
+                    >
+                      {item.title}
+                    </motion.h3>
+                  </Step>
+                  {item.description && (
+                    <motion.p
+                      animate={{
+                        opacity: isInView ? 1 : 0,
+                        y: isInView ? 0 : 10,
+                        transition: {
+                          duration: 0.3,
+                          ease: "easeInOut",
+                          delay: 0.3 * idx,
+                        },
+                      }}
+                      className="pt-1 pl-6 text-sm text-neutral-600 dark:text-neutral-400"
+                    >
+                      {item.description}
+                    </motion.p>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       ))}
     </div>
+  );
+};
+const Step = ({
+  className,
+  children,
+  idx,
+  isInView,
+}: {
+  className: string;
+  children: React.ReactNode;
+  idx: number;
+  isInView: boolean;
+}) => {
+  return (
+    <motion.div
+      animate={{
+        opacity: isInView ? 1 : 0,
+        y: isInView ? 0 : -10,
+        transition: {
+          duration: 0.3,
+          ease: "easeInOut",
+          delay: 0.2 * idx,
+        },
+      }}
+      className={cn("flex items-start gap-2", className)}
+    >
+      <IconCircleCheckFilled className="mt-1 h-4 w-4 text-neutral-500 dark:text-neutral-400" />
+
+      {children}
+    </motion.div>
   );
 };
